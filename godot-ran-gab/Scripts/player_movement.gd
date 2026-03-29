49a57362b4b5e5923e64a436b2810b692e862255
GabrielUnity <ilissongabriel@gmail.com>
2026-03-29 22:13:21 +0300

    Basic character
    Added slime character sprite sheets and basic movement

---
+++ godot-ran-gab/Scripts/player_movement.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var speed = 200
+@export var gravity = 0
+
+#func _input(event: InputEvent) -> void:
+	#if event is InputEventKey:
+		#if event is Key.A
+			#print("left")
+
+func _process(delta):
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		print("right")
+		direction.x = 1
+
+	if Input.is_action_pressed("ui_left"):
+		print("left")
+		direction.x = -1
+		
+	if Input.is_action_pressed("ui_up"):
+		print("right")
+		direction.y = -1
+
+	if Input.is_action_pressed("ui_down"):
+		print("left")
+		direction.y = 1
+	#print(direction)
+	velocity = direction * speed
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	move_and_slide()
